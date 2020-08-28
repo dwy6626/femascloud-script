c92716b28a211804646c9bc1f23a748af6c4a298
David Ye <deweiye6626@gmail.com>
2020-08-28 13:42:21 +0800

    session object

--- main.py
+++ main.py
@@ -1,40 +1,49 @@
 import requests
 from bs4 import BeautifulSoup
-# import getpass
-# import os
+import getpass
+import os
+import keyring
 import re
 
 
-def checkPunchTime(username, passwd) :
-    payload = {
-        'data[Account][username]': username,
-        'data[Account][passwd]': passwd,
-    }
-    response = requests.post('https://femascloud.com/domain/Accounts/login', data = payload)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    button = soup.find_all("input")
-    script = soup.find_all("script", type="text/javascript")
-    print(button)
-    print(script)
-    # mainData = soup.find(id = 'showMainDataDiv')
-    # dragTables = mainData.find_all('td', class_ = 'dragTable')
-    # dragTable = dragTables[3]
-    # row = dragTable.find_all('tr', class_ = 'ebtr')[0]
-    # column = row.find_all('td', class_ = 'ebtd')
-    # today = column[0]
-    # punchIn = column[2]
-    # return 'My punch-in time is: ' + today.get_text() + ' ' + punchIn.get_text()
+class PunchSession:
+    def __init__(self, username):
+        self.username = username
+        self.session = requests.Session()
+        self.logged_in = False
+        self.payload = {
+            'data[Account][username]': self.username,
+            'data[Account][passwd]': self.fetch_password(),
+        }
 
-# passwd = getpass.getpass()
-# message = checkPunchTime(YOUR_USER_NAME, passwd)
-# os.system("osascript -e 'display notification \"" + message + "\"'")
+    def login(self):
+        self.session.post('https://femascloud.com/domain/Accounts/login', data=self.payload) \
+                    .raise_for_status()
+        self.session.headers.update(
+            {'referer': 'https://femascloud.com/domain/users/main?from=/Accounts/login?ext=html'}
+        )
+        self.logged_in = True
+        
+    def fetch_password(self):
+        password = keyring.get_password(domain, self.username)
+        if password is not None:
+            return password
 
-# checkPunchTime(YOUR_USER_NAME, PASS)
+        print("請輸入密碼，密碼將自動儲存")
+        password = getpass.getpass()
+        keyring.set_password(domain, self.username, password)
 
-def parseHTML(url="./response.html"):
-    with open(url, 'r') as f:
-        soup =  BeautifulSoup(f.read(), 'html.parser')
-    script = soup.find_all("script", type="text/javascript", string=re.compile("submit"))
-    print(script)
+        return password
 
-parseHTML()
+    def check_time(self):
+        raise NotImplementedError
+
+    def parseHTML(self, url="./response.html"):
+        with open(url, 'r') as f:
+            soup =  BeautifulSoup(f.read(), 'html.parser')
+        script = soup.find_all("script", type="text/javascript", string=re.compile("submit"))
+
+
+if __name__ == '__main__':
+    session = PunchSession(YOUR_USER_NAME)
+    session.login()
