8975094c77a016af8928411a9f1bc2b6ca93403b
David Ye <deweiye6626@gmail.com>
2020-09-23 16:34:31 +0800

    test schedule

---
+++ test.py
@@ -0,0 +1,54 @@
+import schedule
+import time
+import logging
+from lib import AddableTime
+
+
+logger = logging.getLogger()
+FORMAT = '[%(asctime)s] %(message)s'
+logging.basicConfig(format=FORMAT)
+
+
+def punch_in():
+    logger.warning("I'm in...")
+
+
+def punch_out():
+    logger.warning("I'm out...")
+
+
+def clear_job():
+    schedule.clear('punch_in')
+    schedule.clear('punch_out')
+
+
+def set_job():
+    schedule.every().wednesday.at(
+        str(AddableTime(9, 29).random_before(25))
+    ).do(punch_in).tag('punch_in')
+    schedule.every().thursday.at(
+        str(AddableTime(9, 29).random_before(25))
+    ).do(punch_in).tag('punch_in')
+    schedule.every().friday.at(
+        str(AddableTime(9, 29).random_before(25))
+    ).do(punch_in).tag('punch_in')
+
+    schedule.every().wednesday.at(
+        str(AddableTime(18).random_after(13))
+    ).do(punch_out).tag('punch_out')
+    schedule.every().thursday.at(
+        str(AddableTime(18).random_after(13))
+    ).do(punch_out).tag('punch_out')
+    schedule.every().friday.at(
+        str(AddableTime(18).random_after(13))
+    ).do(punch_out).tag('punch_out') 
+
+
+schedule.every().wednesday.at('09:00').do(set_job)
+schedule.every().friday.at('18:30').do(clear_job)
+
+
+while True:
+    set_job()
+    schedule.run_pending()
+    time.sleep(1)
